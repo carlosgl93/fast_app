93b21aaa7bdd8498d28d4aa58172f7c4b4967aa6
unknown <cgumucio93@gmail.com>
2021-05-22 21:56:28 -0400

    item_repository & item_model done, resume at item_repository.dart, minute 12:55

---
+++ lib/controllers/item_list_controller.dart
@@ -0,0 +1,2 @@
+import 'package:/fast_app/models/item_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
--- lib/general_providers.dart
+++ lib/general_providers.dart
@@ -5,5 +5,5 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final firebaseAuthProvider =
     Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
-final FirebaseFirestoreProvider =
+final firebaseFirestoreProvider =
     Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
